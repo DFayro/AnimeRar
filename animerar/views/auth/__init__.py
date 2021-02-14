67cea2e661ca30cc95b695b814502f18a743a0d8
Fayro <>
2021-02-14 01:12:15 +0100

    [feat]: Forms setup

--- animerar/views/auth/__init__.py
+++ animerar/views/auth/__init__.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, url_for, current_app, Flask, request
-from animerar.core import validation
+from flask import Blueprint, render_template, request
+
 from animerar.core import NavBar
 
 blueprint = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
@@ -11,29 +11,63 @@ def index():
 
 	if request.method == "POST":
 
-		errors = []
-
 		print(request)
 		email = request.form['email']
 		password = request.form['password']
 
-		if not validation.is_email(email):
-			errors.append("Email invalid")
+		error = "Login failed"
 
-		if not validation.is_password(password):
-			errors.append("Password invalid format")
-
-		print(request.form)
-
-		if errors:
-			return render_template("login.html", navbar=navbar, login_errors=errors)
+		if error:
+			return render_template("login.html", navbar=navbar, login_error=error)
 
 	return render_template("login.html", navbar=navbar)
+
 
 @blueprint.route("/register", methods=['GET', 'POST'])
 def register():
 	navbar = NavBar.default_bar()
+
+	if request.method == "POST":
+		valid = True
+
+		errors = {}
+
+		# Validate
+
+		email = request.form.get('email')
+		display_name = request.form.get('display_name')
+		first_name = request.form.get('first_name')
+		last_name = request.form.get('last_name')
+		password = request.form.get('password')
+		repeat_password = request.form.get('repeat_password')
+
+		if not email:
+			errors['email_error'] = "Not a valid address"
+			valid = False
+
+		if not display_name:
+			errors['display_name_error'] = "Not a valid address"
+			valid = False
+
+		if not first_name:
+			errors['first_name_error'] = "Not a valid address"
+			valid = False
+
+		if not last_name:
+			errors['last_name_error'] = "Not a valid address"
+			valid = False
+
+		if not password:
+			errors['password_error'] = "Not a valid address"
+			valid = False
+
+		if not repeat_password:
+			errors['repeat_password_error'] = "Not a valid address"
+			valid = False
+
+		if valid:
+			pass
+
+		return render_template("register.html", navbar=navbar, posted=True, **errors)
+
 	return render_template("register.html", navbar=navbar)
-
-
-
