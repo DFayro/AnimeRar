fe0c8467fbefc8340db5b91de0315fb97c628a9b
Fayro <>
2021-02-18 06:57:53 +0100

    [feat]: Add profile page

--- animerar/views/home/__init__.py
+++ animerar/views/home/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 from animerar.core import NavBar
 
@@ -9,3 +10,10 @@ blueprint = Blueprint("home", __name__, template_folder="templates", static_fold
 def index():
 	navbar = NavBar.default_bar(active_page="Home")
 	return render_template("home.html", navbar=navbar)
+
+
+@blueprint.route("/profile")
+@login_required
+def profile():
+	navbar = NavBar.default_bar()
+	return render_template("profile.html", navbar=navbar)
