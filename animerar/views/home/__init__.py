1b6d56403bf548b73460ea3ac26dda941a53aafd
Fayro <>
2021-02-23 17:24:55 +0100

    [feat]: Add recently added anime to home page

--- animerar/views/home/__init__.py
+++ animerar/views/home/__init__.py
@@ -2,14 +2,17 @@ from flask import Blueprint, render_template
 from flask_login import current_user, login_required
 
 from animerar.core import NavBar
+from animerar.models.anime import Anime
 
 blueprint = Blueprint("home", __name__, template_folder="templates", static_folder="static")
 
 
 @blueprint.route("/")
 def index():
+	recently_added_anime = Anime.query.order_by(Anime.title.desc()).limit(3).all()
+
 	navbar = NavBar.default_bar(active_page="Home")
-	return render_template("home.html", navbar=navbar)
+	return render_template("home.html", navbar=navbar, recently_added_anime=recently_added_anime)
 
 
 @blueprint.route("/profile")
